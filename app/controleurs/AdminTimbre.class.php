db053daac8a960dfa334a1e5752e268630dd61eb
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2022-12-07 13:54:43 -0500

    modification timbre

--- app/controleurs/AdminTimbre.class.php
+++ app/controleurs/AdminTimbre.class.php
@@ -51,8 +51,8 @@ class AdminTimbre extends Admin {
       $oTimbre = new Timbre($timbre);
       
       //var_dump("isi"); exit;
-      //  $erreurs = $oTimbre->erreurs;
-      // if (count($erreurs) === 0){
+       $erreurs = $oTimbre->erreurs;
+      if (count($erreurs) === 0){
         print_r($_POST);
         $timbre_id=$this->oRequetesSQL->ajouterTimbre([
           // 'timbre_nom'      => $_POST['timbre_nom'],
@@ -82,7 +82,7 @@ class AdminTimbre extends Admin {
         }
         $this->listerTimbres(); // retour sur la page de liste des timbres
         exit;
-      // }
+       }
     
      
     }
@@ -103,6 +103,45 @@ class AdminTimbre extends Admin {
    * Modifier un timbre
    */
   public function modifierTimbre() {
+    if (count($_POST) !== 0){
+      $timbre = $_POST;
+      $oTimbre = new Timbre($timbre);
+      $erreurs = $oTimbre->erreurs;
+      if (count($erreurs) === 0) {
+        $retour = $this->oRequetesSQL->modifierTimbre([
+          'timbre_nom'      => $oTimbre->timbre_nom,
+          'timbre_date'   => $oTimbre->timbre_date,
+          'timbre_couleur' => $oTimbre->timbre_couleur,
+          'timbre_tirage'      => $oTimbre->timbre_tirage,
+          'timbre_description'   => $oTimbre->timbre_description,
+          'timbre_prix_plancher'   => $oTimbre->timbre_prix_plancher,
+          'timbre_dimension'   => $oTimbre->timbre_dimension,
+          'timbre_pays_id'   => $oTimbre->timbre_pays_id,
+          'timbre_enchere_id' => $oTimbre->timbre_enchere_id
+        ]);
+        
+          if ($retour === true)  {
+            $this->messageRetourAction = "Modification de timbre numéro $this->timbre_id effectuée.";    
+          } else {  
+            $this->classRetour = "erreur";
+            $this->messageRetourAction = "Modification de timbre numéro $this->timbre_id non effectuée.";
+          }
+          $this->listerTimbres();
+          exit;
+       
+      }
+    } 
+
+    (new Vue)->generer(
+      'vAdminTimbreModifier',
+      [
+        'oUtilConn'   => self::$oUtilConn,
+        'titre'       => "Modifier timbre numéro $this->utilisateur_id",
+        'utilisateur' => $utilisateur,
+        'erreurs'     => $erreurs
+      ],
+      'gabarit-admin');
+
     throw new Exception("Développement en cours.");
   }
   
